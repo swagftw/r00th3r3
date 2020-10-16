fa76d92c11f1bebd75b2e031a3ec31e8ee86010b
swag <swapnilnakade.sn@gmail.com>
2020-10-16 15:52:52 +0530

    changed name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
+      title: 'Counter open app',
       theme: ThemeData(
         // This is the theme of your application.
         //
