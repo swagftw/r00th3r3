6d1afd7763b53ba290c9dd4972b2cf4ac161f64c
swag <swapnilnakade.sn@gmail.com>
2020-10-16 17:25:29 +0530

    added state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/all.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(ProviderScope(child: MyApp()));
+
+class CounterNotifier extends ChangeNotifier {
+  int _count = 0;
+
+  int get getCount => _count;
+
+  void increament() {
+    _count++;
+    notifyListeners();
+  }
 }
+
+final countProvider = ChangeNotifierProvider<CounterNotifier>((_) {
+  return CounterNotifier();
+});
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter open app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: CounterWidget(),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          context.read(countProvider).increament();
+        },
         child: Icon(Icons.add),
-      ), 
+      ),
     );
+  }
+}
+
+class CounterWidget extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final count = watch(countProvider);
+    return Text(count.getCount.toString());
   }
 }
