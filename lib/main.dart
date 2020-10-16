1e677677fc1c27357b5579fd4704d1b9910f97ce
swag <swapnilnakade.sn@gmail.com>
2020-10-16 15:42:27 +0530

    added name

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Open Source Counter App',
+      title: 'Counter App',
       theme: ThemeData(
         // This is the theme of your application.
         //
